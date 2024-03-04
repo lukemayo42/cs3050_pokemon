430c4675af1c8dd1621027a041faf82d1241a447
Eric-Seligmann <eseligma@uvm.edu>
2024-03-04 17:22:13 -0500

    Create controller.py to allow for easier communication between front end and back end

---
+++ controller.py
@@ -0,0 +1,5 @@
+# Imports from both sides. This is where we communicate
+
+# Call function in battle.py that returns the moves of the current pokemon that is out
+
+# send that list to GUI to to create specific buttons

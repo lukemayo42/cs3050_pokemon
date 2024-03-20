f2f356c4a10854ff7f0db8c05ada3127da3282e9
Eric-Seligmann <eseligma@uvm.edu>
2024-03-20 15:51:27 -0400

    Add is_revive variable with getter and setter

--- item.py
+++ item.py
@@ -5,14 +5,15 @@ class item:
     description - description of item - string
     cost - the cost of the item - integer
     health_recovered - the amount of health recovered when the item is used - integer
+    is_revive - a boolean stating whether the item is a revive potion or not
     """
 
-    def __init__(self, name, description, cost, health_recovered):
+    def __init__(self, name, description, cost, health_recovered, is_revive):
         self.name = name
         self.description = description
         self.cost = cost
         self.health_recovered = health_recovered
-
+        self.is_revive = is_revive
     # Getter methods
     def get_name(self):
         return self.name
@@ -26,6 +27,9 @@ class item:
     def get_health_recovered(self):
         return self.health_recovered
 
+    def get_is_revive(self):
+        return self.is_revive
+
     # Setter methods
     def set_name(self, name):
         self.name = name
@@ -38,3 +42,6 @@ class item:
 
     def set_health_recovered(self, health_recovered):
         self.health_recovered = health_recovered
+
+    def set_is_revive(self, is_revive):
+        self.is_revive = is_revive

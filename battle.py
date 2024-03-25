32e7f40b68ee623ab92bbcfafb46b38440e4538f
Eric-Seligmann <eseligma@uvm.edu>
2024-03-25 15:14:55 -0400

    Add in functionality for the enemy to randomly choose an action

--- battle.py
+++ battle.py
@@ -142,22 +142,68 @@ def player_turn(player, enemy, btn_info):
 
 #single turn of an enemy character, takes in character and player 
 def enemy_turn(enemy, player):
-    action_str = "move"
+    enemy_pkm = enemy.get_curr_pkm()
+    player_pkm = player.get_curr_pkm()
+
+    # Randomly pick a number to determine the probability an action is taken.
+    probability_action = random.randint(0, 20)
+    # If the enemy's current pokemon is at 10% health or less, there is an 85% chance to use an item, a 5% chance to
+    # swap pokemon, and a 10% chance to use a move.
+    if enemy_pkm.get_curr_hlth() <= (enemy_pkm.get_max_hlth()/10):
+        if probability_action in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]:
+            action_str = "item"
+        elif probability_action == 0:
+            # Make sure that the enemy player has a pokemon to swap to. If not, change action string to move.
+            if enemy.get_pokemon_list()[1].get_is_fainted() and enemy.get_pokemon_list()[2].get_is_fainted():
+                action_str = "move"
+            else:
+                action_str = "swap"
+        else:
+            action_str = "move"
+    # If the enemy's current pokemon is at 25% health or less, there is an 47.5% chance to use an item, a 5% chance to
+    # swap pokemon, and a 47.5% chance to use a move.
+    elif enemy_pkm.get_curr_hlth() <= (enemy_pkm.get_max_hlth()/4):
+        if probability_action in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+            action_str = "item"
+        elif probability_action == 0:
+            # Make sure that the enemy player has a pokemon to swap to. If not, change action string to move.
+            if enemy.get_pokemon_list()[1].get_is_fainted() and enemy.get_pokemon_list()[2].get_is_fainted():
+                action_str = "move"
+            else:
+                action_str = "swap"
+        else:
+            action_str = "move"
+    # If the enemy's current pokemon is at 50% health or less, there is an 19% chance to use an item, a 5% chance to
+    # swap pokemon, and a 76% chance to use a move.
+    elif enemy_pkm.get_curr_hlth() <= (enemy_pkm.get_max_hlth()/2):
+        if probability_action in [1, 2, 3, 4]:
+            action_str = "item"
+        elif probability_action == 0:
+            # Make sure that the enemy player has a pokemon to swap to. If not, change action string to move.
+            if enemy.get_pokemon_list()[1].get_is_fainted() and enemy.get_pokemon_list()[2].get_is_fainted():
+                action_str = "move"
+            else:
+                action_str = "swap"
+        else:
+            action_str = "move"
+    else:
+        action_str = "move"
+
     action_flag = True
     #randomly choose move
     move_index = random.randint(0,3)
-    move_used = enemy.get_curr_pkm().get_moves()[move_index]
+    move_used = enemy_pkm.get_moves()[move_index]
     if action_str == "move":
         if roll_accuracy(move_used):
             #send gui sometyhing sayinssg it hit 
-            dmg = calc_dmg(enemy.get_curr_pkm(), player.get_curr_pkm(), move_used)
-            player.get_curr_pkm().remove_health(dmg)
-            action = enemy.get_curr_pkm().move_to_string(move_used, True)
-            print(enemy.get_curr_pkm().move_to_string(move_used, True))
+            dmg = calc_dmg(enemy_pkm, player_pkm, move_used)
+            player_pkm.remove_health(dmg)
+            action = enemy_pkm.move_to_string(move_used, True)
+            print(enemy_pkm.move_to_string(move_used, True))
         else:
             #send gui something saying it missed
             action = enemy.get_curr_pkm().move_to_string(move_used, False)
-            print(enemy.get_curr_pkm().move_to_string(move_used, False))
+            print(enemy_pkm.move_to_string(move_used, False))
             action_flag = False
         
     #item
